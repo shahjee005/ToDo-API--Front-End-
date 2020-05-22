8f68655389b97eadc5abb125cb9c38a754c1e42e
Syed-Gardezi <61136718+shahjee005@users.noreply.github.com>
2020-05-22 14:51:07 -0600

    added on click remove item from the list

--- js/scripts.js
+++ js/scripts.js
@@ -2,22 +2,33 @@
 const allToDos = document.getElementById( 'index' );
 // If available, proceed.
 if ( allToDos )
-{ // Run Axios to obtain list.
-  axios.get( 'https://localhost:44300/api/ToDoItems/' )
+{ // Repeatable function for showing all of our to-dos.
+  const outputToDos = () => {
+    // Clear any existing items.
+    allToDos.innerHTML = '';
+    // Run Axios to obtain list.
+    axios.get( 'https://localhost:44300/api/ToDoItems/' )
     // Get just the data from the response.
     .then( response => response.data )
     // "Process" our data (JSON object or array.)
     .then( data => {
       // console.log( data );
       data.forEach( toDo => { // Output list of todos.
-        const toDoLI = document.createElement( 'LI' );toDoLI.textContent = ' ' + toDo.task;
+        const toDoLI = document.createElement( 'LI' );
+        toDoLI.textContent = ' ' + toDo.task;
         const toDoCheckbox = document.createElement( 'INPUT' );
         toDoCheckbox.type = 'checkbox';
+        toDoCheckbox.addEventListener( 'click', event => {
+          axios.delete( 'https://localhost:44300/api/ToDoItems/' + toDo.id )
+            .then( response => { outputToDos(); } );
+        } );
         toDoLI.prepend( toDoCheckbox );
         allToDos.appendChild( toDoLI );
       } );
     } );
-
+  }
+  // On pageload, output the todos!
+  outputToDos();
 }
 
 // Grab the "create" ToDo form.
